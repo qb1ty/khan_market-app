3293920259e71279b4f5673e44031ae3c47b5be0
MacBook <macbook@MacBook-Air-MacBook.local>
2024-04-17 10:55:12 +0500

    edit port to get fetched

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 3000
 
 app.use(express.static('./frontend/build'))
 
-app.listen(port, () => console.log('server has been started on port 80'))
+app.listen(port, () => console.log('server has been started on port 3000'))
